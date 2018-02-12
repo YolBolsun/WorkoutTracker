4bb5270a689ca858a617591ced98d6af421f1b53
Jonathan Clinton <jonathanclinton@Jonathans-MacBook-Pro.local>
2018-02-11 18:09:49 -0600

    Initial Commit

---
+++ WorkoutTracker/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  WorkoutTracker
+//
+//  Created by Jonathan Clinton on 2/11/18.
+//  Copyright © 2018 Jonathan Clinton. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ WorkoutTracker/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  WorkoutTracker
+//
+//  Created by Jonathan Clinton on 2/11/18.
+//  Copyright © 2018 Jonathan Clinton. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
